58698af1b14a219a492cd1fb3e19b63531c23fa8
Fred Gomez <fred.titao@gmail.com>
2020-07-17 04:14:41 +0200

    Imp. ValidationSchema

--- src/schema/ValidationSchema.ts
+++ src/schema/ValidationSchema.ts
@@ -1,9 +1,12 @@
 import { PropertyValidator } from '../validation/PropertyValidator'
 
-export interface ValidationSchema<T = unknown> {
+export interface ValidationSchemaOptions {
   name?: string
   allowUnknownProperties?: boolean
   allowMissingProperties?: boolean
+}
+
+export interface ValidationSchema<T = unknown> extends ValidationSchemaOptions {
   properties: {
     [K in keyof Required<T>]: PropertyValidator | PropertyValidator[] | ValidationSchema<T[K]>
   }
