7d84667388ccb3f359b901cb3dd3b8fcfeb3a7e3
Vincent Mullier <vincent.mullier@gmail.com>
2020-07-23 11:06:19 +0200

    cleanup - remove all specific decorators

--- src/decorator/IsOptional.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/ban-types */
-import { IsOptional as IsOptionalValidator } from '../validator/IsOptional'
-import { SchemaBuilder } from './Validate'
-
-export function IsOptional(): PropertyDecorator {
-  return function IsOptional(target: Object, propertyKey: string | symbol) {
-    SchemaBuilder.of(target.constructor).useValidator(propertyKey, IsOptionalValidator())
-  }
-}
